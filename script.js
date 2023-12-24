f87e082b0ae0254617daa0e176a58b9f860c4361
Rayyan Sohaib <124036859+Gigadev99@users.noreply.github.com>
2023-12-24 10:46:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,31 @@ const dialog = document.querySelector("dialog")
 const submit = document.querySelector("form button")
 button.onclick = () => dialog.showModal()
 let addbook = e => {
-    e.preventDefault();
-    let title = document.querySelector("#title")
-    let author = document.querySelector("#author").textContent
-    let pages = document.querySelector("#pages").textContent
-    let isRead = document.querySelector("#isread")
-    document.body.innerHTML += `<div class="row">
-    <div class="title">${title.textContent}</div>
+    let title = document.querySelector("#title").value
+    let author = document.querySelector("#author").value
+    let pages = document.querySelector("#pages").value
+    let isRead = document.querySelector("#isread").checked
+    let checked = "";
+    if (isRead == true) {
+      checked = "checked"
+    }
+    dialog.close()
+    document.body.insertAdjacentHTML("beforeend",
+    `<div class="row">
+    <div class="title">${title}</div>
     <div class="author">${author}</div>
     <div class="pages">${pages}</div>
-    <label class="isread" for="0">Read</label><input type="checkbox">
-    </div>`
-    dialog.close()
+    <label class="isread" for="0">Read<input type="checkbox" ${checked}></label>
+    <button class="remove">×</button>
+    </div>`)
+    deleteRows()
+    
 }
 submit.addEventListener('click', addbook)
+document.querySelector(".close").onclick = () => dialog.close()
+function deleteRows() {
+  let remove = document.querySelectorAll(".remove")
+  remove.forEach(button => button.addEventListener('click', e => e.target.parentNode.remove()))
+}
+deleteRows
 
